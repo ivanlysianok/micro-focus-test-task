d4d9b0b0225f427c055a39740c5dff6debf6cc1f
Ivan Lysianok <ivanlysianok@gmail.com>
2022-11-11 21:37:40 +0100

    Bootstrap + Material Angular installing, configurations

---
+++ frontend/src/app/modules/home-page/home-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HomePageComponent } from './components/home-page.component';
+
+@NgModule({
+  declarations: [HomePageComponent],
+  imports: [],
+  exports: [HomePageComponent],
+})
+export class HomePageModule {}
